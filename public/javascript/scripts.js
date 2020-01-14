689f81b0be0eaf5dd12d32f54adebba2a790094d
FLUX <pj.flux2001@gmail.com>
2020-01-14 19:12:54 +0530

    Reversal to less thick borders

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,11 +1,11 @@
 function bigImg(x) {
     x.className = "ui card bg-info zoom";
-    x.style.border = "5px solid black";
+    x.style.border = "2px solid black";
   }
   
   function normalImg(x) {
     x.className = "ui card";
-    x.style.border = "2.5px solid black";
+    x.style.border = "1px solid black";
   }
 
 
