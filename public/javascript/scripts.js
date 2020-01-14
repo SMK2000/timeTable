7d693cca7af9b5b5aa1ebd388211918973bd46ab
FLUX <pj.flux2001@gmail.com>
2020-01-14 14:58:06 +0530

    zoom+border

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,9 +1,11 @@
 function bigImg(x) {
     x.className = "ui card bg-info zoom";
+    x.style.border = "5px solid black";
   }
   
   function normalImg(x) {
     x.className = "ui card";
+    x.style.border = "2.5px solid black";
   }
 
 
